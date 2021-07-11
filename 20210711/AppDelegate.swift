dfb0d9441447598ef82cf40bcba4dac42ebc2342
mmm <dsa1520@naver.com>
2021-07-11 15:03:09 +0900

    Add alert

--- 20210711/AppDelegate.swift
+++ 20210711/AppDelegate.swift
@@ -7,15 +7,49 @@
 
 import UIKit
 
+// 추상화
 @main
+
+// App 
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    //     App life cycle
 
-
+    // app이 최초 실행된 직후
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+//        print("didFinishLaunchingWithOptions")
+        
         // Override point for customization after application launch.
         return true
     }
+    
+    
+    // app이 active 상태로된 직후
+//    func applicationDidBecomeActive(_ application: UIApplication) {
+//        print("applicationDidBecomeActive")
+//    }
+//
+//    // app이 inactive 상태로 전환되기 직전
+//    func applicationWillResignActive(_ application: UIApplication) {
+//        print("applicationWillResignActive")
+//    }
+//
+//    // app이 배그라운드 상태로 전환되기 직전
+//    func applicationDidEnterBackground(_ application: UIApplication) {
+//        print("applicationDidEnterBackground")
+//    }
+//
+//    // app이 active 상태가 되기 직전 && 화면에 보여지기 직전
+//    func applicationWillEnterForeground(_ application: UIApplication) {
+//        print("applicationWillEnterForeground")
+//    }
+//
+//    // app이 종료되기 직전
+//    func applicationWillTerminate(_ application: UIApplication) {
+//        print("applicationWillTerminate")
+//    }
+
 
     // MARK: UISceneSession Lifecycle
 
--- 20210711/ViewController.swift
+++ 20210711/ViewController.swift
@@ -9,11 +9,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // view life cycle
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var subTItleLabel: UILabel!
+    
+    
+    
+    //load 되었을떄
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func btnTab(_ sender: Any) {
+        titleLabel.text = "changed Title"
+        subTItleLabel.text = "changed subTitled"
+    }
+    
+    
+    @IBAction func btnAction(_ sender: Any) {
+        
+        
+        //style = 앞에 생략후 .만으로 가능
+        //controller = modal
+        let alertVC = UIAlertController(title: "active", message: "actived", preferredStyle: .actionSheet)
+        
+        //action = action btn of modal
+        let okBtn = UIAlertAction(title: "OK", style: .default, handler: nil)
+        
+        let cancelBtn = UIAlertAction(title: "cancel", style: .cancel, handler: nil)
+        
+        let settingBtn = UIAlertAction(title: "setting", style: .destructive, handler: nil)
+        
+        alertVC.addAction(okBtn)
+        alertVC.addAction(cancelBtn)
+        alertVC.addAction(settingBtn)
+        
+        present(alertVC, animated: true, completion: nil)
+        
+    }
 }
 
